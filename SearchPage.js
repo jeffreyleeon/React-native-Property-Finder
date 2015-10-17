20954aea121db13b01eaf9b093d0c379b4d64fe2
JeffreyLee <jeffreyleeon@gmail.com>
2015-10-17 15:40:32 +0800

    Add detail search result page and geolocation search

--- SearchPage.js
+++ SearchPage.js
@@ -102,6 +102,22 @@ class SearchPage extends Component {
     this._executeQuery(query);
   }
 
+  onLocationPressed() {
+    navigator.geolocation.getCurrentPosition(
+      location => {
+        var search = location.coords.latitude + ',' + location.coords.longitude;
+        this.setState({ searchString: search });
+        var query = urlForQueryAndPage('centre_point', search, 1);
+        this._executeQuery(query);
+      },
+      error => {
+        this.setState({
+          message: 'There was a problem with obtaining your location: ' + error
+        });
+      }
+    );
+  }
+
   _executeQuery(query) {
     console.log(query);
     this.setState({ isLoading: true });
@@ -162,7 +178,8 @@ class SearchPage extends Component {
         </View>
 
         <TouchableHighlight style={styles.button}
-            underlayColor='#99d9f4'>
+            underlayColor='#99d9f4'
+            onPress={this.onLocationPressed.bind(this)}>
           <Text style={styles.buttonText}>Location</Text>
         </TouchableHighlight>
 
