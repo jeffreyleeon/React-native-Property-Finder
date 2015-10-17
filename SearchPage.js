65e3b1f28d1592544a9caeedaaca9fb3f5c49d3a
JeffreyLee <jeffreyleeon@gmail.com>
2015-10-17 14:31:29 +0800

    Able to show search results

--- SearchPage.js
+++ SearchPage.js
@@ -1,6 +1,7 @@
 'use strict';
  
 var React = require('react-native');
+var SearchResults = require('./SearchResults');
 var {
   StyleSheet,
   Text,
@@ -63,31 +64,89 @@ var styles = StyleSheet.create({
   }
 });
 
+function urlForQueryAndPage(key, value, pageNumber) {
+  var data = {
+      country: 'uk',
+      pretty: '1',
+      encoding: 'json',
+      listing_type: 'buy',
+      action: 'search_listings',
+      page: pageNumber
+  };
+  data[key] = value;
+ 
+  var querystring = Object.keys(data)
+    .map(key => key + '=' + encodeURIComponent(data[key]))
+    .join('&');
+ 
+  return 'http://api.nestoria.co.uk/api?' + querystring;
+};
+
 class SearchPage extends Component {
   constructor(props) {
       super(props);
       this.state = {
-          searchString: 'london'
+          searchString: 'london',
+          isLoading: false,
+          message: ''
       };
   }
 
   onSearchTextChanged(event) {
-      console.log('onSearchTextChanged');
       this.setState({ searchString: event.nativeEvent.text });
       console.log(this.state.searchString);
   }
 
+  onSearchPressed() {
+    var query = urlForQueryAndPage('place_name', this.state.searchString, 1);
+    this._executeQuery(query);
+  }
+
+  _executeQuery(query) {
+    console.log(query);
+    this.setState({ isLoading: true });
+    // Do the actual fetching
+    fetch(query)
+      .then(response => response.json())
+      .then(json => this._handleResponse(json.response))
+      .catch(error => 
+         this.setState({
+          isLoading: false,
+          message: 'Something bad happened ' + error
+       }));
+  }
+
+  _handleResponse(response) {
+    this.setState({ isLoading: false , message: '' });
+    if (response.application_response_code.substr(0, 1) === '1') {
+      console.log('response is = ' + response);
+      console.log('response listings is = ' + response.listings);
+      this.props.navigator.push({
+        title: 'Results',
+        component: SearchResults,
+        passProps: {listings: response.listings}
+      });
+    } else {
+      this.setState({ message: 'Location not recognized; please try again.'});
+    }
+  }
 
   render() {
-    console.log('SearchPage.render');
+    var spinner = this.state.isLoading ?
+      ( <ActivityIndicatorIOS
+          hidden='true'
+          size='large'/> ) :
+      ( <View/>);
     return (
       <View style={styles.container}>
         <Text style={styles.description}>
           Search for houses to buy!
         </Text>
+
         <Text style={styles.description}>
           Search by place-name, postcode or search near your location.
         </Text>
+
         <View style={styles.flowRight}>
           <TextInput
             style={styles.searchInput}
@@ -95,15 +154,23 @@ class SearchPage extends Component {
             onChange={this.onSearchTextChanged.bind(this)}
             placeholder='Search via name or postcode'/>
           <TouchableHighlight style={styles.button}
-              underlayColor='#99d9f4'>
+              underlayColor='#99d9f4'
+              onPress={this.onSearchPressed.bind(this)}
+              >
             <Text style={styles.buttonText}>Go</Text>
           </TouchableHighlight>
         </View>
+
         <TouchableHighlight style={styles.button}
             underlayColor='#99d9f4'>
           <Text style={styles.buttonText}>Location</Text>
         </TouchableHighlight>
+
         <Image source={require('image!house')} style={styles.image}/>
+
+        {spinner}
+
+        <Text style={styles.description}>{this.state.message}</Text>
       </View>
     );
   }
